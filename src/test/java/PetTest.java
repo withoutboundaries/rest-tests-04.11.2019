a83a870e51dabe0a95191bb430ab2f855605eed2
Iryna Gnatyshko <gnatyshko.i@gmail.com>
2019-11-29 08:35:25 +0100

    HW#7

--- src/test/java/PetTest.java
+++ src/test/java/PetTest.java
@@ -1,108 +1,108 @@
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+
+import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
 import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
+
 
 import static org.hamcrest.core.AnyOf.anyOf;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class PetTest {
-    static long petId;
+    private PetEndpoint petEndpoint = new PetEndpoint();
+    private static long petId;
+    private enum Status {AVAILABLE, PENDING, SOLD};
+    Status myvar=Status.AVAILABLE;
 
-    public RequestSpecification given() {
-     return   RestAssured
-                .given()
-             .baseUri("https://petstore.swagger.io/v2")
-                .log().all()
-                .contentType(ContentType.JSON)
-                ;
 
-    }
+    private String body = "{\n" +
+            "  \"id\": 0,\n" +
+            "  \"category\": {\n" +
+            "    \"id\": 0,\n" +
+            "    \"name\": \"cat\"\n" +
+            "  },\n" +
+            "  \"name\": \"Nopetty\",\n" +
+            "  \"photoUrls\": [\n" +
+            "    \"string\"\n" +
+            "  ],\n" +
+            "  \"tags\": [\n" +
+            "    {\n" +
+            "      \"id\": 0,\n" +
+            "      \"name\": \"string\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"status\": \"available\"\n" +
+            "}";
+
 
     @Before
-    public void test1CreatePet() {
-
-        String body = "{\n" +
-                "  \"id\": 0,\n" +
-                "  \"category\": {\n" +
-                "    \"id\": 0,\n" +
-                "    \"name\": \"cat\"\n" +
-                "  },\n" +
-                "  \"name\": \"Nopetty\",\n" +
-                "  \"photoUrls\": [\n" +
-                "    \"string\"\n" +
-                "  ],\n" +
-                "  \"tags\": [\n" +
-                "    {\n" +
-                "      \"id\": 0,\n" +
-                "      \"name\": \"string\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"status\": \"available\"\n" +
-                "}";
+    public void beforeMethod() {
+        ValidatableResponse response = petEndpoint
+                .createPet(body)
+                .statusCode(anyOf(is(200), is(201)))
+                .body("category.name", is(not("")))
+                .body("id", is(not("")));
+        petId = response.extract().path("id");
+    }
 
 
-        ValidatableResponse response = given()
-                .body(body)
-                .post(PetEndpoint.CREATE_PET)
-                .then()
+    @Test
+    public void createPet() {
+        petEndpoint
+                .createPet(body)
                 .statusCode(anyOf(is(200), is(201)))
                 .body("category.name", is(not("")))
                 .body("id", is(not("")))
-                .log().all();
-        response.extract().body().path("")
         ;
-
-        String petName = response.extract().path("name");
-        System.out.println(petName);
-
-        petId = response.extract().path("id");
-        System.out.println(petId);
-
     }
 
-
     @Test
-    public void test2GetPetById() {
-        System.out.println(petId);
-
-        given()
-                .get(PetEndpoint.GET_PET, petId)
-                .then()
+    public void getPetById() {
+        petEndpoint
+                .getPet(petId)
                 .statusCode(anyOf(is(200), is(201)))
                 .body("category.name", is(not("")))
-                .log().all()
         ;
     }
 
 
     @Test
-    public void test3DeletePetById() {
-        System.out.println(petId);
+    public void deletePetById() {
 
-       given()
-                .delete(PetEndpoint.DELETE_PET, petId)
-                .then()
+        petEndpoint
+                .deletePet(petId)
                 .statusCode(anyOf(is(200), is(201)))
-                .log().all()
         ;
 
-        given()
-                .get(PetEndpoint.GET_PET, petId)
-                .then()
+        petEndpoint
+                .getPet(petId)
                 .statusCode(is(404))
-                .log().all()
         ;
+    }
 
-
+    @Test
+    public void findPetByStatus() {
+         petEndpoint
+                .findPet(myvar)
+                .statusCode(is(200));
 
     }
+
+    @Test
+    public void updatePet () {
+
+        Body <String,String> newBody = new Body<>();
+        newBody.put("name", "BLA");
+        newBody.put("category.name", "fox");
+
+        petEndpoint
+                .updatePet(newBody)
+                .statusCode(is(200))
+        ;
+    }
+
 
 
 }
